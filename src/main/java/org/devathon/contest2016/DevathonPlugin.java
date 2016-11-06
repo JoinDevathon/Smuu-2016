6523dc5342556c8d6116b560b94ac9fd972e4570
samuwitz <sender1397@gmail.com>
2016-11-06 11:10:00 +0100

    platform create

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -1,17 +1,119 @@
 package org.devathon.contest2016;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathonPlugin extends JavaPlugin {
 
+    private Location spawnLocation = new Location(Bukkit.getWorlds().get(0), 0, 200, 0);
+
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+        initWorld();
+
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    private void initWorld() {
+
+        spawnLocation.getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, 1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(-1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(0, 0, -1).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        spawnLocation.add(1, 0, 0).getBlock().setType(Material.COBBLESTONE);
+        
+        spawnLocation.setX(0);
+        spawnLocation.setZ(0);
+    }
+
+    public Location getSpawnLocation() {
+        return spawnLocation;
     }
 }
 
--- src/main/java/org/devathon/contest2016/PlayerListener.java
+++ src/main/java/org/devathon/contest2016/PlayerListener.java
@@ -1,5 +1,6 @@
 package org.devathon.contest2016;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -7,8 +8,18 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
+    private final DevathonPlugin plugin;
+
+    public PlayerListener(DevathonPlugin devathonPlugin) {
+        this.plugin = devathonPlugin;
+    }
+
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        player.teleport(plugin.getSpawnLocation().add(0.5, 1, 0.5));
+
         event.setJoinMessage(null);
     }
 
